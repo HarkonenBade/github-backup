da2a7a890572e70f8eba34d35a389f02910a7096
Thomas Bytheway <github@harkonen.net>
2017-05-15 22:17:24 +0100

    Slight tweaks, neatening things a bit

--- github-backup.py
+++ github-backup.py
@@ -14,14 +14,18 @@ DEFAULT_CONF_PATH = "./github-backup.yml"
 
 DEFAULT_CONF_BODY = """
 general:
-{token}
   repopath: "{repopath}"
-# Number of unknown repos required to prompt a warning in cron mode. (Default: 5, set to 0 to disable)
+{token}
+# Number of unknown repos required to prompt a warning in non-interactive mode. (Default: 5, set to 0 to disable)
 # unknown_repo_warning: 5
 # Only look for repos owned by the authed user (Default: true)
 # only_personal: false
 repos: {{}}
 exclude: []"""
+
+
+def sprint(txt, *args, **kwargs):
+    print(txt.format(*args, **kwargs), flush=True)
 
 
 def gen_default_conf(conf_path, token):
@@ -69,88 +73,99 @@ def conf_load(conf, *args, default=None):
 
 def update_repos(repos, repopath):
     for name, repo in repos.items():
-        print("Updating {}...".format(name))
+        sprint("Updating {}...", name)
         g_repo = git.Repo(os.path.join(repopath, name, ''))
         if repo['clone_url'] != g_repo.remotes.origin.url:
-            print("Repo url is incorrect, altering.")
+            sprint("Repo url is incorrect, altering.")
             g_repo.remotes.origin.set_url(repo['clone_url'], g_repo.remotes.origin.url)
         g_repo.remotes.origin.fetch()
 
 
 def clone_repo(repo, repopath):
+    sprint("Cloning repo {} from {}", repo['name'], repo['clone_url'])
     git.Repo.clone_from(repo['clone_url'], os.path.join(repopath, repo['name']), mirror=True)
+
+
+def check_unknown(unknown_repos, repopath):
+    sprint("{} unknown repos found while checking github, please classify them:", len(unknown_repos))
+    new_repos = {}
+    new_exclude = []
+    for repo in unknown_repos:
+        sprint("Name: {} Fork?: {}",
+               repo['name'],
+               "Yes" if repo['fork'] else "No")
+        while True:
+            choice = input("(A)dd/(S)kip/(E)xclude? ").upper()
+            if choice in ["A", "S", "E"]:
+                break
+        if choice == "A":
+            sprint("Adding {} to the list of repos.", repo['name'])
+            new_repos[repo['name']] = {'clone_url': repo['clone_url']}
+            clone_repo(repo, repopath)
+        elif choice == "E":
+            sprint("Adding {} to the exclusion list.", repo['name'])
+            new_exclude.append(repo['name'])
+        else:
+            sprint("Skipping {} for this run.", repo['name'])
+    return new_repos, new_exclude
 
 
 def gather_args():
     parser = argparse.ArgumentParser(description="A github mirroring tool.")
     parser.add_argument("--conf", default=DEFAULT_CONF_PATH)
     parser.add_argument("--token", default="")
-    parser.add_argument("--cron", action="store_true")
-    parser.add_argument("--gen-conf", action="store_true")
+    parser.add_argument("--interactive", action="store_true")
     return parser.parse_args()
 
 
 def main():
     args = gather_args()
 
-    if args.gen_conf:
+    if not os.path.exists(args.conf):
+        sprint("No config file found, generating a bare-bones one.")
         gen_default_conf(args.conf, args.token)
+
+    with open(args.conf, "r") as conf_file:
+        conf = yaml.safe_load(conf_file)
+
+    if args.token != "":
+        auth = args.token
     else:
-        with open(args.conf, "r") as conf_file:
-            conf = yaml.safe_load(conf_file)
-
-        if args.token != "":
-            auth = args.token
-        else:
-            auth = conf_load(conf, 'general', 'token')
-            if auth is None:
-                print("Error: Must either specify auth token on command line or in config.")
-                sys.exit(1)
-        ghub = GitHub(token=auth)
-
-        repopath = conf_load(conf, 'general', 'repopath')
-        if repopath is None:
-            print("Error: Config must specify a repopath in the general section.")
+        auth = conf_load(conf, 'general', 'token')
+        if auth is None:
+            sprint("Error: Must either specify auth token on command line or in config.")
             sys.exit(1)
+    ghub = GitHub(token=auth)
 
-        only_personal = conf_load(conf, 'general', 'only_personal', default=True)
-        unknown_repo_warning = conf_load(conf, 'general', 'unknown_repo_warning', default=5)
-        exclude = conf_load(conf, 'exclude', default=[])
-        conf_repos = conf_load(conf, 'repos', default=[])
-        ghub_repos = load_repos(ghub, only_personal, exclude)
+    repopath = conf_load(conf, 'general', 'repopath')
+    if repopath is None:
+        sprint("Error: Config must specify a repopath in the general section.")
+        sys.exit(1)
 
-        unknown = [repo for name, repo in ghub_repos.items() if name not in conf_repos]
+    only_personal = conf_load(conf, 'general', 'only_personal', default=True)
+    unknown_repo_warning = conf_load(conf, 'general', 'unknown_repo_warning', default=5)
+    exclude = conf_load(conf, 'exclude', default=[])
+    conf_repos = conf_load(conf, 'repos', default={})
+    ghub_repos = load_repos(ghub, only_personal, exclude)
 
-        if not args.cron:
-            for repo in unknown:
-                print("Unknown Repo\n"
-                      "Name: {}\n"
-                      "Fork: {}".format(repo['name'], "Yes" if repo['fork'] else "No"))
-                while True:
-                    choice = input("(A)dd/(S)kip/(E)xclude? ").upper()
-                    if choice in ["A", "S", "E"]:
-                        break
-                if choice == "A":
-                    print("Adding {} to the list of repos.".format(repo['name']))
-                    conf_repos[repo['name']] = {'clone_url': repo['clone_url']}
-                    clone_repo(repo, repopath)
-                elif choice == "E":
-                    print("Adding {} to the exclusion list.".format(repo['name']))
-                    exclude.append(repo['name'])
-                else:
-                    print("Skipping {} for this run.".format(repo['name']))
-            conf['repos'] = conf_repos
-            conf['exclude'] = exclude
-            with open(args.conf, "w") as conf_file:
-                yaml.safe_dump(conf, conf_file)
+    unknown = [repo for name, repo in ghub_repos.items() if name not in conf_repos]
 
-        update_repos(conf_repos, repopath)
+    if args.interactive:
+        new_repos, new_exclude = check_unknown(unknown, repopath)
+        conf['repos'] += new_repos
+        conf['exclude'] += exclude
+        with open(args.conf, "w") as conf_file:
+            yaml.safe_dump(conf, conf_file)
 
-        if args.cron:
-            if 0 < unknown_repo_warning <= len(unknown):
-                print("Error: There are {} unknown repos on github. "
-                      "This is more than your limit of {}.".format(unknown, unknown_repo_warning))
-                sys.exit(2)
+    update_repos(conf_repos, repopath)
+
+    if not args.interactive:
+        if 0 < unknown_repo_warning <= len(unknown):
+            sprint("Error: There are {} unknown repos on github. "
+                   "This is more than your limit of {}.",
+                   unknown,
+                   unknown_repo_warning)
+            sys.exit(2)
 
 
 if __name__ == "__main__":
